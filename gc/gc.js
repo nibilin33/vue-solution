96684706925f9ff4dbb1165fd53c2e932a7a353f
nibilin <819231957@qq.com>
2020-02-22 13:37:28 +0800

    update

--- gc/gc.js
+++ gc/gc.js
@@ -1,6 +1,8 @@
 /* 
-循环引用，组件A即使用B的方法又使用组件B中ztree的方法，组件B导入ztree，this上有挂载着new zree（this）,  ztree里使用B的方法    
+循环引用，组件A即使用B的方法又使用组件B中ztree的方法，组件B导入ztree（依赖挂到了window上），this上有挂载着new zree（this）,  ztree里使用B的方法    
 A
-↓ ➘
-B⇆ ztree 
+↓ ➘ 
+B⇆ ztree
+B⇆ztree 相互引用，当组件A被注销的后，就无论通过什么途径均访问不到它们，    
+就无法被注销。          
 */
--- src/custom-close.js
+++ src/custom-close.js
@@ -1,27 +1,43 @@
 class CustomClose extends HTMLElement {
-    triple=null;
-    constructor() {
-        super();
-        this.addEventListener('click', e => {
-          if(e.target.className === 'ripple') {
-            this.parentElement.removeChild(this);
-            this.triple = null;
-          }
-        });
-        this.addEventListener('mouseover',e=>{
-            this.toggleRipple();
-        });
+  triple = null;
+  constructor() {
+    super();
+    this.addEventListener('click', e => {
+      if (e.target.className === 'ripple') {
+        this.parentElement.removeChild(this);
+        this.triple = null;
       }
-      toggleRipple() {
-        if(this.triple) {
-            return;
-        }
-        let div = document.createElement('div');
-        div.classList.add('ripple');
-        div.innerHTML = 'x';
-        this.triple = div;
-        this.appendChild(this.triple);
-      }
+    });
+    this.addEventListener('mouseover', e => {
+      this.toggleRipple();
+    });
+  }
+  // 当元素被加入html后
+  connectedCallback(){
+    console.log('connectedCallback');
+  }
+  // 当元素从html被删除
+  disconnectedCallback(){
+    console.log('disconnectedCallback');
+  } 
+  // 监听元素属性变化
+  attributeChangedCallback() {
+    console.log('attributeChangedCallback');
+  }
+  // 从当前document移动到另一document，比如iframe移动到主文档   
+  adoptedCallback() {
+    console.log('adoptedCallback');
+  }
+  toggleRipple() {
+    if (this.triple) {
+      return;
+    }
+    let div = document.createElement('div');
+    div.classList.add('ripple');
+    div.innerHTML = 'x';
+    this.triple = div;
+    this.appendChild(this.triple);
+  }
 }
 
 window.customElements.define('nb-close', CustomClose);
