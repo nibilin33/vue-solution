2d0b64adbf8a4c02fe226f7060f33787a934b906
nibilin <819231957@qq.com>
2020-02-27 09:15:47 +0800

    refactor

--- webpack-plu-loader/source.js
+++ webpack-plu-loader/source.js
@@ -1,1 +1,1 @@
-"/js/0.js","/js/1.js","/js/2.js","/js/3.js","/3.b42bd3db06328cd5a7e6.hot-update.js","/js/app.js","/js/chunk-vendors.js"
+"/js/0.js","/js/1.js","/js/2.js","/js/3.js","/js/app.js","/js/chunk-vendors.js"
