fc5d5a681a33fb19e806bbf55cc82b7f1a22c248
nibilin <819231957@qq.com>
2020-03-02 14:18:18 +0800

    update loader

--- webpack-plu-loader/i18nloader.js
+++ webpack-plu-loader/i18nloader.js
@@ -1,7 +1,11 @@
+const loaderUtils = require("loader-utils");
 module.exports = function (source) {
+    let options = loaderUtils.getOptions(this);
+    console.log(options);
+    // todo:额外替换规则
     try {
         let result = source.match(/\<template\>[\s\S]*\<\/template\>/gmi).map((it)=>{
-            return it.replace(/(\{\{\$t\(.*\)\}\})/g,'<nb-close>$1</nb-close>');
+            return it.replace(/(\{\{\$t\((.*)\)\}\})/g,'<nb-close data-key=$2>$1</nb-close>');
         }).join('');
        return source.replace(/\<template\>([\s\S]*)\<\/template\>/gmi,result);
     } catch (error) {
