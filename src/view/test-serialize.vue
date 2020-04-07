24acc867f244ae2d845310629a27d3f33b75574a
nibilin <819231957@qq.com>
2020-04-07 18:25:50 +0800

    update

--- src/view/test-serialize.vue
+++ src/view/test-serialize.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="border">
     数据包：<br />
-    <p>{{ sendRepeat }}</p>
-    <p />
+    <p class="border-limit">{{ sendRepeat }}</p>
+    <p/>
     <el-form label-width="100" size="small">
       <el-form-item label="字段重复扩展/次">
-        <el-input-number v-model="msRepeat" :min="10"></el-input-number>
+        <el-input-number v-model="msRepeat" :min="10" :max="500"></el-input-number>
       </el-form-item>
       <el-form-item label="发送频率（次/秒）">
         <el-input-number v-model="num" :min="10" :max="2000"></el-input-number>
@@ -41,8 +41,8 @@ import request from "@/request.js";
 import message from "./messages.js";
 const  sendData = {
         id: "12345",
-        time: new Date().valueOf().toString(),
-        message: "test".repeat(1000),
+        time: "1",
+        message: "test",
         password: "OEUIW",
 };
 export default {
@@ -70,6 +70,10 @@ export default {
       this.protoResult = "";
     },
     send() {
+      if(window.location.protocol.startsWith('https')) {
+          this.$message('不支持https,需要起本地服务');
+          return;
+      }
       for (let i = 0; i < this.num; i++) {
         setTimeout(() => {
           request.post("/v1/json", this.sendRepeat).then((res) => {
@@ -98,5 +102,10 @@ export default {
 <style lang="scss" scoped>
 .border {
   padding: 5px 5px;
+}
+.border-limit{
+    word-break: break-word;
+    max-height: 400px;
+    overflow-y:auto;
 }
 </style>
